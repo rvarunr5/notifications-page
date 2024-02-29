92aa27aa76513a2d1208c05d397cd469a3d856e5
Varun Reddy Regalla <rvarunr5@gmail.com>
2024-02-29 16:51:06 -0500

    js code and made mobile compatible

---
+++ README-template.md
@@ -0,0 +1,26 @@
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- Distinguish between "unread" and "read" notifications
+- Select "Mark all as read" to toggle the visual state of the unread notifications and set the number of unread messages to zero
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+
+### Screenshot
+
+![](./screenshot.png)
+
+### Links
+
+- [Solution URL](https://your-solution-url.com)
+- [Live Site URL](https://your-live-site-url.com)
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,59 @@
 import "../css/style.css";
-// import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap')
+const notificationCount = document.querySelector(".notification-count");
+const notificationElements = document.querySelectorAll(".notification-item");
+const markAllAsReadItem = document.querySelector(".mark-as-read");
+
+class Notifications {
+  constructor(notificationElements, notificationCount, markAllAsReadItem) {
+    this.notificationElements = notificationElements;
+    this.notificationCount = notificationCount;
+    this.markAllAsReadItem = markAllAsReadItem;
+    this.count = this.notificationElements.length;
+  }
+
+  makeNotificationsAsUnread() {
+    this.notificationElements.forEach((item) => {
+      item.classList.add("unread");
+      const notificationContent = item.querySelector(".notification-content");
+      notificationContent.innerHTML += "<span class='bullet'>&bull;</span>";
+    });
+  }
+  updateNotificationItem(item, readAll = false) {
+    if (item.classList.contains("unread")) {
+      item.classList.remove("unread");
+      const bullet = item.querySelector("span.bullet");
+      if (bullet) bullet.remove();
+      this.count = readAll ? 0 : this.count - 1;
+      this.updateNotificationCount(this.count);
+    }
+  }
+  updateNotificationCount() {
+    this.notificationCount.innerHTML = this.count;
+  }
+  markAllAsRead() {
+    if (this.count > 0) {
+      this.notificationElements.forEach((item) => {
+        this.updateNotificationItem(item, true);
+      });
+    }
+  }
+  init() {
+    this.makeNotificationsAsUnread();
+    this.updateNotificationCount();
+    this.notificationElements.forEach((item) => {
+      item.addEventListener("click", () => {
+        this.updateNotificationItem(item);
+      });
+    });
+    this.markAllAsReadItem.addEventListener("click", () => {
+      this.markAllAsRead();
+    });
+  }
+}
+const notifications = new Notifications(
+  notificationElements,
+  notificationCount,
+  markAllAsReadItem
+);
+
+notifications.init();
